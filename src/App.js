776c1628e8c79dd915f2ceab22b1fed9b3442fdb
Anne Hesemann <annehesemann@annes-mbp.lan>
2021-05-22 18:14:24 -0700

    Adding inputs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+// import reactDom from 'react-dom';
 
 function App() {
+
+  const [year, setYear] = useState('2001');
+  const [month, setMonth] = useState('05');
+  const [day, setDay] = useState('01');
+
+
+  function onMonthChange(ev){
+    let value = ev.target.value;
+    setMonth(value);
+  }
+
+  function onDayChange(ev){
+    let value = ev.target.value;
+    setDay(value);
+  }
+
+  function onYearChange(ev){
+    let value = ev.target.value;
+    setYear(value);
+  }
+  
+  function doFetch(){
+    console.log('fetching');
+    fetch('https://api.ratesapi.io/api/' + year +'-'+ month +'-' + day)
+    .then(response => response.json())
+    .then(data => {
+      console.log('data came back', data);
+     return;
+   
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class="BarChart-title"> Currency Exchange Rates (Base EUR) </div>
+      <h1>Please enter a date on or after January 4, 1998 to see the exchange rate:</h1>
+      <div className="BarChartInput">
+      {
+      <input placeholder="Month mm"
+      value={month}
+      OnChange={onMonthChange}
+      />
+     }
+
+     {
+      <input placeholder="Day dd"
+      value={day}
+      OnChange={onDayChange}
+      />
+     }
+     
+     {
+      <input placeholder="Year yyyy"
+      value={year}
+      OnChange={onYearChange}
+      />
+     }
+    
+    <button onClick={doFetch}>Submit</button>
+      
+   </div>
+  </div>
   );
-}
+
+  }
 
 export default App;
